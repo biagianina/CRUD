b75fd28b29acc50ac8f4ca1e3d5ba24746314d66
bianca gianina <biagianina02@gmail.com>
2019-09-03 18:21:40 +0300

    implemented Add function

---
+++ CRUD/IntArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD
+{
+    public class IntArray
+    {
+        private int[] intArray;
+
+        public IntArray()
+        {
+            intArray = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            Array.Resize(ref intArray, intArray.Length + 1);
+            intArray[intArray.Length - 1] = element;
+        }
+
+        public int[] GetArray()
+        {
+            return intArray;
+        }
+        
+    }
+}
---
+++ CRUDTests/IntArrayTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+
+namespace CRUD
+{
+    public class IntArrayTests
+    {
+        [Fact]
+        public void AddAnElement()
+        {
+            var array = new IntArray();
+            int[] expected = { 1 };
+            array.Add(1);
+
+            Assert.Equal(expected, array.GetArray());
+        }
+
+
+        [Fact]
+        public void AddTwoElements()
+        {
+            var array = new IntArray();
+            int[] expected = { 1 , 2 };
+            array.Add(1);
+            array.Add(2);
+            Assert.Equal(expected, array.GetArray());
+        }
+    }
+}
